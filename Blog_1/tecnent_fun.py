a30132f80000403d887b7002207d9c2bd22f38d7
haitao <wanghaitao@i.shu.edu.cn>
2018-03-09 16:50:50 +0800

    第一遍博客的代码，正在修改中

--- Blog_1/tecnent_fun.py
+++ Blog_1/tecnent_fun.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import numpy as np
-df=pd.read_csv('dataset/data.csv')
+import matplotlib.pyplot as plt
+df=pd.read_csv('../dataset/data.csv')
 
 col=['gender','company','school','job','location']
 
@@ -25,3 +25,16 @@ for name,group in df_job.groupby('company'):
 
     # print(name)
     # print(group)
+import matplotlib.pyplot as plt
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 55, 10]
+explode = (0, 0.1, 0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()

481f70188e911d440478644f2a321562db4fd5a2
wanghaitao <wanghaitao@i.shu.edu.cn>
2018-03-18 14:14:36 +0800

    添加第二篇博客代码

--- Blog_2/draw.py
+++ Blog_2/draw.py
@@ -48,6 +48,29 @@ school_col=['school','ask_num','follower','following','agree_num','answer_num']
 df_school=pd.DataFrame(data=list2,columns=school_col)
 df_school=df_school.sort_values('follower',ascending=False)
 print(df_school)
-df_school.to_csv('../dataset/dataBlog2Location.csv',header=school_col,index=False)
+df_school.to_csv('../dataset/dataBlog2School.csv',header=school_col,index=False)
 print("data preprosessing ok!")
-        
+
+list3=[]
+for name,group in df.groupby('trade'):
+    if len(group)>100:
+        vet_3=[]
+        vet_3.append(name)
+        vet_3.append(group['ask_num'].sum())
+        vet_3.append(group['follower'].sum())
+        vet_3.append(group['following'].sum())
+        vet_3.append(group['agree_num'].sum())
+        vet_3.append(group['answer_num'].sum())
+        # print(name,'ask_num',group['ask_num'].sum())
+        # print(name,'follower',group['follower'].sum())
+        # print(name,'following',group['following'].sum())
+        # print(name,'agree_num',group['agree_num'].sum())
+        # print(name,'answer_num',group['answer_num'].sum())
+        # print(vet_1)
+        list3.append(vet_3)
+trade_col=['trade','ask_num','follower','following','agree_num','answer_num']
+df_trade=pd.DataFrame(data=list3,columns=trade_col)
+df_trade=df_trade.sort_values('follower',ascending=False)
+print(df_trade)
+df_trade.to_csv('../dataset/dataBlog2trade.csv',header=trade_col,index=False)
+print("data preprosessing ok!")
